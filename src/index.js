1bb9817ca7f8be73e73ed8efed13cc716e63c82e
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 09:27:08 +0530

    CSS style as variable

--- src/index.js
+++ src/index.js
@@ -2,28 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-
 //create react component
 const App = () => {
-  const buttonText="Click Me";
-  const buttonText1=1234;
-  const buttonText2=["Hii","Hello"];
-  const buttonText3=[20,15];
-  const buttonText4={
-    text:'Click on me'
+  const buttonStyle = {
+    backgroundColor: "cyan",
+    color: "white",
   };
-
   return (
     <div>
-      <p>{buttonText}</p>
-      <p>{buttonText1}</p>
-      <p>{buttonText2}</p>
-      <p>{buttonText2[0]}</p>
-      <p>{buttonText3}</p> 
-      {/* <p>{buttonText4}</p>  */}
-      {/* gives error that you can not refer to JS object */}
-      <p>{buttonText4.text}</p>     
+      <button style={buttonStyle}>Click Me</button>
     </div>
   );
 };
