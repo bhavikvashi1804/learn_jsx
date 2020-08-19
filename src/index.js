e13c3598734509635a66b8c7287966b902ffbb0b
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 08:59:24 +0530

    JSX can reference to function to get the value and display

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText(){
+  return ("Click Me");
+}
+
 //create react component
 const App = () => {
-  const buttonText="Click Me";
   return (
     <div>
       <label className="label" for="name">
         Enter your name:
       </label>
       <input type="text" id="name" />
-      <button type="submit" style={{backgroundColor:'red', color:'white'}}>{buttonText}</button>
+      <button type="submit" style={{backgroundColor:'red', color:'white'}}>{getButtonText()}</button>
       
     </div>
   );
